8b449b4e317633ed4edd3e4e2b84f5f9781895e2
Jonathan Ferreira <jferr@jsferrei.(none)>
2013-07-23 12:35:49 -0400

    added rounding feature

--- histo.py
+++ histo.py
@@ -3,10 +3,16 @@ import random
 import os
 import sys
 
-def gen_histo(vector, nbins=1, increment = 0, compress = True):
+def gen_histo(vector, nbins=1, increment = 0, compress = True, round_label = 0):
     rows, columns = os.popen('stty size', 'r').read().split()
-    bins,edges = numpy.histogram(vector, bins=nbins) 
-    edges = [str(edges[i])+' - '+str(edges[i+1])+':' for i in xrange(len(edges)-1)]
+    bins,edges = numpy.histogram(vector, bins=nbins)
+    prepend = {1:'+', 0:' ', -1:''}
+    if round_label > 0:
+        #edges = numpy.round(edges,round_label) 
+        rounder_string = "%0."+str(round_label)+'f'
+        edges = [prepend[cmp(edges[i],0)]+rounder_string % edges[i]+' to '+prepend[cmp(edges[i+1],0)]+rounder_string % edges[i+1]+':' for i in xrange(len(edges)-1)]
+    else:
+        edges = [prepend[cmp(edges[i],0)]+str(edges[i])+' to '+prepend[cmp(edges[i+1], 0)]+str(edges[i+1])+':' for i in xrange(len(edges)-1)]
     max_edge = max([len(edge) for edge in edges])
     sum_b = numpy.sum(bins)
     bins = numpy.float32(bins)
@@ -30,7 +36,8 @@ def print_columns(*args):
 
 def main(argv):
     example = [random.normalvariate(1, .5) for x in xrange(1000000)]
-    gen_histo(example,20, 1, False)
+    gen_histo(example,20, 1, False, 10)
+    gen_histo(example,20, 1, False, 0)
     
 if __name__ == '__main__':
     main(sys.argv[1:])
