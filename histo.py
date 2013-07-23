eac5ff97e60b50bb23df7bc56ed358e23a0359c6
Jonathan Ferreira <jferr@jsferrei.(none)>
2013-07-23 12:08:30 -0400

    included compress feature

--- histo.py
+++ histo.py
@@ -5,16 +5,18 @@ import sys
 
 def gen_histo(vector, nbins=1, increment = 0, compress = True):
     rows, columns = os.popen('stty size', 'r').read().split()
-    max_v = max(vector)
-    bin_size = (max_v)/nbins
     bins,edges = numpy.histogram(vector, bins=nbins) 
     edges = [str(edges[i])+' - '+str(edges[i+1])+':' for i in xrange(len(edges)-1)]
     max_edge = max([len(edge) for edge in edges])
     sum_b = numpy.sum(bins)
     bins = numpy.float32(bins)
     bins /= sum_b
-    bins *= (int(columns)-max_edge)
+    bins *= (int(columns)-max_edge) #to help with wrapping
     bins = numpy.int32(bins)
+    if compress:
+        bins_remove = [i for i in xrange(len(bins)) if bins[i] == 0]
+        edges = [edges[i] for i in xrange(len(edges)) if i not in bins_remove]
+        bins = numpy.array([bins[i] for i in xrange(len(bins)) if i not in bins_remove])
     bins += increment
     bins = ['*'*b for b in bins]
     print_columns(edges, bins)
